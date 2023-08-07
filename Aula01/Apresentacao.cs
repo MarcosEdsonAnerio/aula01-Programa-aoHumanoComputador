ce23dd91845f45b1569b60cdee51184b32305943
helya <helya@HELYKSSIA>
2023-08-07 20:50:47 -0300

    Adicionar arquivos de projeto.

---
+++ Aula01/Apresentacao.cs
@@ -0,0 +1,35 @@
+﻿Atividades função = new Atividades();
+
+
+Console.WriteLine("\nESCOLHA DE LISTA DE EXERCICIOS");
+
+while (true)
+{
+    Console.WriteLine("\nEscolha a opção desejada:");
+    Console.WriteLine("1 - Exercicio 1");
+    Console.WriteLine("2 - Exercicio 2");
+    Console.WriteLine("3 - Exercicio 3");
+    Console.WriteLine("0 - Sair");
+
+    Console.Write("\nOpção: ");
+    int opcao = int.Parse(Console.ReadLine());
+
+    if (opcao == 0)
+    {
+        break;
+    }
+
+    switch (opcao)
+    {
+        case 1:
+            função.Exercicio1();
+            break;
+
+        case 2:
+            função.Exercicio2();
+            break;
+        case 3:
+            função.Exercicio3();
+            break;
+    }
+}
---
+++ Aula01/Atividades.cs
@@ -0,0 +1,113 @@
+﻿internal class Atividades
+{
+        public class Funcionario
+        {
+            public string nome;
+
+            public string cpf;
+
+            public string matricula;
+
+            public double salario;
+
+            public void Bonificacao()
+            {
+                salario += 500;
+            }
+        }
+        public void Exercicio1()
+        {
+            Funcionario fun1 = new Funcionario();
+            Funcionario fun2 = new Funcionario();
+
+            Console.WriteLine("Digite seu Nome: ");
+            fun1.nome = Console.ReadLine();
+            Console.WriteLine("Digite seu CPF: ");
+            fun1.cpf = Console.ReadLine();
+            Console.WriteLine("Digite sua Matricula: ");
+            fun1.matricula = Console.ReadLine();
+            Console.WriteLine("Digite seu Salario: ");
+            fun1.salario = Convert.ToDouble(Console.ReadLine());
+
+            fun1.Bonificacao();
+
+            Console.WriteLine("\nFUNCIONARIO 2");
+            Console.WriteLine("\nDigite seu Nome: ");
+            fun2.nome = Console.ReadLine();
+            Console.WriteLine("Digite seu CPF: ");
+            fun2.cpf = Console.ReadLine();
+            Console.WriteLine("Digite sua Matricula: ");
+            fun2.matricula = Console.ReadLine();
+            Console.WriteLine("Digite seu Salario: ");
+            fun2.salario = Convert.ToDouble(Console.ReadLine());
+
+            fun2.Bonificacao();
+
+            if (fun1.salario > fun2.salario)
+            {
+                Console.WriteLine("O funcionário"+fun1.nome+"possui o maior salário: " + fun1.salario);
+            }
+            else if (fun2.salario > fun1.salario)
+            {
+                Console.WriteLine("O funcionário"+fun1.nome+"possui o maior salário: " + fun2.salario);
+            }
+            else
+            {
+                Console.WriteLine("Ambos os funcionários possuem salários iguais: " + fun1.salario);
+            }
+        }
+        public class Triangulo
+        {
+            public double LadoA;
+            public double LadoB;
+            public double LadoC;
+        
+            public double CalcularArea()
+            {
+                double perimetro = (LadoA + LadoB + LadoC) / 2;
+
+                double area = Math.Sqrt(perimetro * (perimetro - LadoA) * (perimetro - LadoB) * (perimetro - LadoC));
+                return area;
+            }
+        }
+        public void Exercicio2()
+        {
+            {
+                Triangulo triangulo = new Triangulo();
+
+                Console.WriteLine("Digite a medida do Lado A do triângulo:");
+                triangulo.LadoA = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Digite a medida do Lado B do triângulo:");
+                triangulo.LadoB = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Digite a medida do Lado C do triângulo:");
+                triangulo.LadoC = Convert.ToDouble(Console.ReadLine());
+
+                double areaTriangulo = triangulo.CalcularArea();
+                Console.WriteLine("A área do triângulo é: " + areaTriangulo);
+            }
+        }
+        public class Circulo
+        {
+            public double Raio;
+
+        public double CalcularArea()
+        {
+            double area = Math.PI * Math.Pow(Raio, 2);
+            return area;
+        }
+        }
+        public void Exercicio3()
+        {
+        {
+            Circulo circulo = new Circulo();
+
+            Console.WriteLine("Digite o valor do raio do círculo:");
+            circulo.Raio = Convert.ToDouble(Console.ReadLine());
+
+            double areaCirculo = circulo.CalcularArea();
+            Console.WriteLine("A área do círculo é: " + areaCirculo);
+        }
+    }
+}
